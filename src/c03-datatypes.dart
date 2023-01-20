c1d3c5f62e5bf47c11b5f70263592de53e51f2da
Simon Cateau <sorani.dev@outlook.com>
2023-01-20 13:38:47 +0100

    Chapter 3 - Data Types in Dart

---
+++ src/c03-datatypes.dart
@@ -0,0 +1,28 @@
+void main() {
+  /**
+   * Data Types:
+   * Number, String, Boolean, List, Map, Dynamic
+   */
+
+  // String
+  String firstName = "Simon";
+  print("String: $firstName");
+
+  // Integer
+  int myNum = 42;
+  print("Integer: $myNum");
+
+  // Double
+  double myDouble = 3.1459;
+  print("Double: $myDouble");
+
+  // Boolean
+  bool canVote = true;
+  bool canDrive = false;
+  print("Boolean: Can vote: $canVote");
+  print("Boolean: Can drive: $canDrive");
+
+  // Dynamic
+  dynamic fullName = "John Doe";
+  print("Dynamic: $fullName");
+}
